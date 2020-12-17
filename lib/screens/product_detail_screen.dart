77998355bfdaacdb0cdbf338969a168501dc82ce
djdeo <deoseoer@gmail.com>
2020-12-17 10:09:35 +0800

    feat: add product detail screen

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -15,8 +15,19 @@ class ProductDetailScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(loadedProduct.title),
       ),
-      body: Center(
-        child: Text(loadedProduct.description),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(loadedProduct.imageUrl, fit: BoxFit.cover,),
+            ),
+            SizedBox(height: 10,),
+            Center(child: Text('\$${loadedProduct.price}', style: TextStyle(fontSize: 20.0, color: Colors.orange[900]),),),
+            Container(child: Center(child: Text(loadedProduct.description, softWrap: true,),),),
+          ],
+        )
       ),
     );
   }
